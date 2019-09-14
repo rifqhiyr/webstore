43416d024f58fde5663c1cf17c0722dcd59a4f45
Yudi Kaka <yudikrisnandi22@gmail.com>
2019-09-14 22:47:01 +0700

    [Yudi] - directory setup

---
+++ src/components/BaseLoading.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "../assets/scss/BaseLoading.scss";
+
+const BaseLoading = props => {
+  const [load, setLoad] = useState(false);
+
+  const hadleLoad = e => {
+    setLoad(!load);
+  };
+
+  return (
+    <button onClick={hadleLoad} className="button-primary">
+      {load ? (
+        <span>
+          <i className="fa fa-circle-o-notch fa-spin" /> loading...
+        </span>
+      ) : (
+        "click for loading"
+      )}
+    </button>
+  );
+};
+
+export default BaseLoading;
