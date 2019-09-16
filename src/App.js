b6b43e42baa4b44350c747807494134f41109864
rifqi <rifqhiyr@gmail.com>
2019-09-16 14:36:00 +0700

    [Rifqi] - create feature login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import BaseLoading from "./components/BaseLoading";
+import SigninComponent from "./components/SigninComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>store</h1>
       <BaseLoading />
+      <SigninComponent />
     </div>
   );
 }
---
+++ src/components/SigninComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "../assets/scss/SigninComponent.scss";
+
+class SigninComponent extends Component {
+  render() {
+    return (
+      <div className="login-page">
+        <div className="form">
+          <h1>sign in</h1>
+          <form className="login-form">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <p>
+              <input type="checkbox" />
+              Remember
+            </p>
+            <button>Sign in</button>
+            <p>
+              <a href="">forgot password</a>
+              <a href="">Have an account</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SigninComponent;
