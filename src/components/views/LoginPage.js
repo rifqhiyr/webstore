5db2d079d628024b6e385f8b67a5e59f0dad0d31
Yudi Kaka <yudikrisnandi22@gmail.com>
2019-09-17 06:41:12 +0700

    [Yudi] - add feature from feature/login branch

--- src/components/views/LoginPage.js
+++ src/components/views/LoginPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import SigninComponent from "../SigninComponent";
+import AuthHeader from "../AuthHeader";
+import Header from "../Header";
+import Footer from "../Footer";
 
 export default class LoginPage extends Component {
   render() {
     return (
       <div>
+        <AuthHeader />
+        <Header />
         <SigninComponent />
+        <Footer />
       </div>
     );
   }
--- src/components/views/RegisterPage.js
+++ src/components/views/RegisterPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import SignupComponent from "../SignupComponent";
+import AuthHeader from "../AuthHeader";
+import Header from "../Header";
+import Footer from "../Footer";
 
 export default class RegisterPage extends Component {
   render() {
     return (
       <div>
+        <AuthHeader />
+        <Header />
         <SignupComponent />
+        <Footer />
       </div>
     );
   }
