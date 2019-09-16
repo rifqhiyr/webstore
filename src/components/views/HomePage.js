30c82746ae716b54fed7331a64df8e50a706d9cc
king <agungdwiputra160@gmail.com>
2019-09-17 00:04:23 +0700

    [Agung] - adding jumbotron in component finish

--- src/components/views/HomePage.js
+++ src/components/views/HomePage.js
@@ -5,6 +5,7 @@ import Footer from "../Footer"
 // Import Components {Dont forget delete in production}
 import AuthHeader from "../homepage/AuthHeader"
 import Header from "../homepage/Header"
+import Jumbotron from "../homepage/Jumbotron"
 
 export default class HomePage extends Component {
   state = {
@@ -19,6 +20,7 @@ export default class HomePage extends Component {
         <h1>header</h1>
         <AuthHeader backColorFooter={this.state.backColorHeader}/>
         <Header backColorHeaderPage={this.state.backColorHeaderPage}/>
+        <Jumbotron/>
         <Footer backColorFooter={this.state.backColorFooter} />
       </div>
     );
