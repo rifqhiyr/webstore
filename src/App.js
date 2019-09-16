44c445efebc8507e3f376946f0f95e79d9248d77
Yudi Kaka <yudikrisnandi22@gmail.com>
2019-09-16 11:29:54 +0700

    [Yudi] - setup routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
-import "./App.css";
-import BaseLoading from "./components/BaseLoading";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./components/views/HomePage";
+import LoginPage from "./components/views/LoginPage";
+import RegisterPage from "./components/views/RegisterPage";
+import DetailPage from "./components/views/DetailPage";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>store</h1>
-      <BaseLoading />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/register" exact component={RegisterPage} />
+        <Route path="/detail" exact component={DetailPage} />
+      </Router>
+    );
+  }
 }
 
 export default App;
