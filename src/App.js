f1ad5456d7ab0afb93ec82b6382846c323faafa8
Fresly L. Sipahutar <freslylumban@gmail.com>
2019-09-16 11:33:47 +0700

    [Fresly] - add component and scss from Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import BaseLoading from "./components/BaseLoading";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>store</h1>
       <BaseLoading />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../assets/scss/Footer.scss";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footer--dark">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer;
