92b651b69c765fd0324eefe23beb7e25780a6e40
alfianinda <ni.alfianinda@gmail.com>
2019-09-16 17:18:18 +0700

    [Alfianinda] - adding formreview and productreview on detailpage component

--- src/components/HotProduct.js
+++ src/components/HotProduct.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 import "../assets/scss/HotProduct.scss";
+import Product1 from "./../assets/images/afghan-flower.gif";
 
 export default class HotProduct extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h2 className="padd">You may also like</h2>
+        <div className="wrap">
+          <div className="flex">
+            <img src={Product1} alt="Product1" />
+            <h5>New look T-Shirt In Gradient Fade</h5>
+            <h5>Rp 140.000</h5>
+            <button className="butt">Submit</button>
+          </div>
+          <div className="flex">
+            <img src={Product1} alt="Product1" />
+            <h5>New look T-Shirt In Gradient Fade</h5>
+            <h5>Rp 140.000</h5>
+            <button className="butt">Submit</button>
+          </div>
+          <div className="flex">
+            <img src={Product1} alt="Product1" />
+            <h5>New look T-Shirt In Gradient Fade</h5>
+            <h5>Rp 140.000</h5>
+            <button className="butt">Submit</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
