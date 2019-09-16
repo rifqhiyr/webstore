fa32e9ebe6bd6afaa94539a022c6ad3c475e8a42
Fresly L. Sipahutar <freslylumban@gmail.com>
2019-09-16 13:46:26 +0700

    [Fresly] - finish component footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,37 @@ import React, { Component } from "react";
 import "../assets/scss/Footer.scss";
 
 class Footer extends Component {
+  state = {
+    warna: true
+  }
   render() {
     return (
       <div className="footer">
-        <div className="footer--dark">
-          
+        <div className={this.state.warna?"footer--dark":"footer--light"}>
+          <ul className="footer--link">
+            <li><a href="">Women</a></li>
+            <li><a href="">Men</a></li>
+            <li><a href="">Kids</a></li>
+            <li><a href="">Shoes</a></li>
+            <li><a href="">Brand</a></li>
+          </ul>
+          <ul className="footer--link__socmed">
+            <li><a href=""><i class="fa fab fa-instagram"></i></a></li>
+            <li><a href=""><i class="fa fab fa-pinterest"></i></a></li>
+            <li><a href=""><i class="fa fab fa-twitter"></i></a></li>
+            <li><a href=""><i class="fa fab fa-facebook"></i></a></li>
+          </ul>
+          <p className="footer--copyright">
+            <small>
+            &copy;2016 Shopmate Ltd
+            </small>
+            <small>
+              • Contact
+            </small>
+            <small>
+              • Privacy policy
+            </small>
+          </p>
         </div>
       </div>
     )
