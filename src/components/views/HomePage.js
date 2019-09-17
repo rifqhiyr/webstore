fb6ca551db720a04796a417419f4005cc29c9f80
alfianinda <ni.alfianinda@gmail.com>
2019-09-17 14:00:42 +0700

    [Alfianinda] - adding revise of classname

--- src/components/views/HomePage.js
+++ src/components/views/HomePage.js
@@ -6,8 +6,9 @@ import AuthHeader from "../AuthHeader";
 import Header from "../Header";
 import Jumbotron from "../Jumbotron";
 import Card from "../Card";
-import HotProduct from "../HotProduct";
 import Hero from "../Hero";
+
+import DataHot from "./../../assets/data/HotData";
 
 export default class HomePage extends Component {
   state = {
@@ -16,13 +17,28 @@ export default class HomePage extends Component {
     backColorFooter: true
   };
   render() {
+    const dataList = DataHot.map(data => {
+      return (
+        <div className="content">
+          <img src={data.img} alt="Product1" />
+          <div className="h5">{data.title}</div>
+          <div className="price">Rp {data.price} ,00</div>
+          <div className="button-wrap">
+            <button className="button-hot">Buy now</button>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div>
         <AuthHeader backColorFooter={this.state.backColorHeader} />
         <Header backColorHeaderPage={this.state.backColorHeaderPage} />
         <Jumbotron />
         <Card />
-        <HotProduct />
+        <div className="padding">
+          <div className="wrap margin-top">{dataList}</div>
+        </div>
         <Hero />
         <Footer backColorFooter={this.state.backColorFooter} />
       </div>
