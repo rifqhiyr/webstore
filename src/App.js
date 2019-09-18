948402e6c9ba442f3cab863f376b86b436dbc6de
Yudi Kaka <yudikrisnandi22@gmail.com>
2019-09-18 10:17:39 +0700

    [Yudi] - add detail component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import HomePage from "./components/views/HomePage"
-import LoginPage from "./components/views/LoginPage"
-import RegisterPage from "./components/views/RegisterPage"
-import DetailPage from "./components/views/DetailPage"
-import CheckoutPage from "./components/views/CheckoutPage"
-import CardPage from "./components/views/CartPage"
-import ProductDetail from "./components/ProductDetail"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./components/views/HomePage";
+import LoginPage from "./components/views/LoginPage";
+import RegisterPage from "./components/views/RegisterPage";
+import DetailPage from "./components/views/DetailPage";
+import CheckoutPage from "./components/views/CheckoutPage";
+import CardPage from "./components/views/CartPage";
 
 class App extends Component {
   render() {
@@ -18,10 +17,9 @@ class App extends Component {
         <Route path="/detail" exact component={DetailPage} />
         <Route path="/cart" exact component={CardPage} />
         <Route path="/checkout" exact component={CheckoutPage} />
-        <ProductDetail />
       </Router>
-    )
+    );
   }
 }
 
-export default App
+export default App;
